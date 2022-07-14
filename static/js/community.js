b7e8c868a175460222e9b9298b4e4ac5af720b05
GyeongSuKim <rla04722@naver.com>
2022-07-14 18:58:12 +0900

    Feat : 업로드 모달 기본 뼈대

---
+++ static/js/community.js
@@ -0,0 +1,14 @@
+// 모달
+$(function () {
+    $("#modal-open").click(function () {
+        $("#popup").css('display', 'flex').hide().fadeIn();
+    });
+
+    $("#close").click(function () {
+        modalClose(); 
+    });
+
+    function modalClose() {
+        $("#popup").fadeOut();
+    }
+});

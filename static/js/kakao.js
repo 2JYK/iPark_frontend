ef3f847450c94444e078ef3ef6841bdb78977beb
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-16 00:49:33 +0900

    Update : 카카오로그인 rename 및 코드정리
    1. 불필요한 코드 삭제
    2. Rename : login_kakao.html -> login.html
    3. 사용자 정보 입력창 기본 숨김 처리

--- static/js/kakao.js
+++ static/js/kakao.js
@@ -3,28 +3,26 @@ const frontend_base_url = "http://127.0.0.1:5500"
 
 // 로그인 //
 function kakaoLogin() {
-  const kakaoLogin = document.getElementById("kakaologin")
+  const kakaoLogin = document.getElementById("kakao-login")
   const userInfo = document.getElementById("user-info")
 
   if (Kakao.isInitialized() == false) {
-    Kakao.init("c414e5945e36386b3b383a30f1b31271");
+    Kakao.init("c414e5945e36386b3b383a30f1b31271")
   }
   Kakao.Auth.login({
-    success: function (authObj) {
-      console.log(authObj)
+    success: function () {
       Kakao.API.request({
         url: "/v2/user/me",
         success: function (response) {
-          console.log("성공", response);
+          console.log("성공", response)
           const kakaoUsername = response.id
           const kakaoEmail = response.kakao_account.email
           
           kakaoLogin.style.visibility = "hidden"
           userInfo.style.visibility = "visible"
-          makeLoginForm(kakaoUsername, kakaoEmail);
+          makeLoginForm(kakaoUsername, kakaoEmail)
         },
         fail: function (error) {
-          console.log("실패", error);
           if (!Kakao.Auth.getAccessToken()) {
             alert("로그인상태가 아닙니다")
             return
@@ -33,50 +31,43 @@ function kakaoLogin() {
             alert("로그아웃 성공" + Kakao.Auth.getAccessToken())
           })
         }
-      });
+      })
     },
     fail: function (err) {
       alert(JSON.stringify(err))
     },
-  });
+  })
 }
 
 
 // 로그아웃 //
 function kakaoLogout() {
-  console.log("확인1")
   if (!Kakao.Auth.getAccessToken()) {
-    console.log('Not logged in.');
-    return;
+    alert("로그아웃 성공")
+    return
   }
   Kakao.Auth.logout(function (response) {
-    alert(response + ' logout');
-    //window.location.href='/'
-  });
-};
+    alert(response + " logout")
+  })
+}
 
 
-// 연결 끊기 //
+// 회원 탈퇴 //
 function disconnect() {
-  console.log("확인2")
   Kakao.API.request({
     url: '/v1/user/unlink',
     success: function (response) {
-      console.log(response);
-      //callback(); //연결끊기(탈퇴)성공시 서버에서 처리할 함수
-      //window.location.href='/'
+      alert("회원탈퇴 성공")
     },
     fail: function (error) {
-      console.log("탈퇴 실패", error);
+      alert("회원탈퇴가 이미 처리되었습니다")
     },
-  });
-};
+  })
+}
 
 
 // 카카오 로그인 성공 -> 받아온 정보값을 회원가입 입력창으로 내용 보내기 (id, email 값) //
 function makeLoginForm(kakaoUsername, kakaoEmail) {
-  console.log(kakaoUsername, kakaoEmail)
-
   const username = document.getElementById("username")
   const email = document.getElementById("email")
 

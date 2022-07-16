50951a0a541f698170fdf473039ec9c666b7b55c
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-16 18:35:02 +0900

    Feat : 로그인 기능 구현

--- static/js/user.js
+++ static/js/user.js
@@ -32,3 +32,40 @@ async function handleSignup() {
         alert("각 항목을 조건에 맞춰 입력해주세요.")
     }
 }
+
+
+// 로그인 //
+async function handleLogin() {
+    const loginData = {
+        username: document.getElementById("floatingInput").value,
+        password: document.getElementById("floatingPassword").value,
+    }
+
+    const response = await fetch(`${backend_base_url}user/api/token/`, {
+        headers: {
+            Accept: "application/json",
+            'Content-type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(loginData)
+    })
+
+    response_json = await response.json()
+
+    if (response.status == 200) {
+        localStorage.setItem("access", response_json.access);
+        localStorage.setItem("refresh", response_json.refresh);
+
+        const base64Url = response_json.access.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(
+            function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+
+        localStorage.setItem("payload", jsonPayload);
+        window.location.replace(`${frontend_base_url}index.html`)
+    } else {
+        alert("잘못된 로그인입니다.", response.status)
+    }
+}

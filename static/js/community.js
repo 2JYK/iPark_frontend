a0aacd5366dd235d478b7f67847f578c22c535be
GyeongSuKim <rla04722@naver.com>
2022-07-18 15:46:34 +0900

    Feat : user_id 함수

--- static/js/community.js
+++ static/js/community.js
@@ -1,3 +1,36 @@
+// 로그인한 user.id 찾는 함수
+function parseJwt(token) {
+  var base64Url = localStorage.getItem("access")
+  if (base64Url != null) {
+    base64Url = base64Url.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(window.atob(base64).split("").map(
+      function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(""));
+      return JSON.parse(jsonPayload);
+    }
+  };
+
+
+// 로그인하지 않은 유저 업로드 금지   << 이거 나중에 지울거임 !
+const modal = document.getElementById("modal-open")
+modal.addEventListener('click', () => {
+  if (parseJwt("access") == null) {
+    alert("로그인을 해주세요.")
+    location.reload();
+  }
+})
+
+
+// 접속 유저 id 확인
+if (parseJwt("access") != null) {
+  console.log("접속한 user_id : ", parseJwt("access").user_id)
+} else {
+  console.log("로그인을 하지 않은 상태")
+}
+
+
 // 모달
 $(function () {
   $("#modal-open").click(function () {

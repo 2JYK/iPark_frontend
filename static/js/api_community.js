600572ce8b6d3a9af3dfe738580c7b3c019a7224
GyeongSuKim <rla04722@naver.com>
2022-07-18 12:13:58 +0900

    Fix : 파일업로드 버그 해결

--- static/js/api_community.js
+++ static/js/api_community.js
@@ -1,6 +1,20 @@
 const backend_base_url = "http://127.0.0.1:8000/"
 const frontend_base_url = "http://127.0.0.1:5500/"
-console.log("ㅡㅡㅡㅡㅡ 접속 완료 ㅡㅡㅡㅡㅡ")
+console.log("접속한 user_id : ", parseJwt('access').user_id)
+
+
+// 로그인한 user.id 찾는 함수 //
+function parseJwt(token) {
+  var base64Url = localStorage.getItem("access").split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(
+    function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+  return JSON.parse(jsonPayload);
+};
+
 
 // 게시글 POST
 async function article_post() {
@@ -11,24 +25,31 @@ async function article_post() {
   const content = document.getElementById("popup-body-content").value
   const formData = new FormData()
 
+  console.log("user :", parseJwt('access').user_id)
   console.log("이미지 :", image)
   console.log("태그 :", tag)
   console.log("태그 :", typeof (tag))
   console.log("제목 :", title)
   console.log("내용 :", content)
 
-  formData.append("image", image)
+  formData.append("image", image[0])
   formData.append("tag", tag)
   formData.append("title", title)
   formData.append("content", content)
 
   const response = await fetch(`${backend_base_url}community/`, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': 'Bearer ' + localStorage.getItem("access"),
+    },
     method: "POST",
     body: formData
   })
   response_json = await response.json()
+  console.log(response_json)
   if (response.status == 200) {
     alert("게시글 작성 완료")
+    location.reload();
   }
   else {
     alert(response_json["message"])

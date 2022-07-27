36e324e34615afd9233732cee4d4dd713ee6bc47
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-27 15:35:17 +0900

    Refactor : 전역함수 user.js 로 이동

--- static/js/park.js
+++ static/js/park.js
@@ -6,21 +6,6 @@ if (!urlParkCommentPageNum) {
 }
 
 
-// 로그인한 user.id 찾는 함수
-function parseJwt(token) {
-  var base64Url = localStorage.getItem("access")
-  if (base64Url != null) {
-    base64Url = base64Url.split(".")[1]
-    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/")
-    var jsonPayload = decodeURIComponent(window.atob(base64).split("").map(
-      function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2)
-      }).join(""))
-    return JSON.parse(jsonPayload)
-  }
-}
-
-
 // 공원 상세정보 html 구간
 function appendParkHtml(
   park_name, addr, check_count, image,
@@ -256,29 +241,6 @@ $(document).ready(function () {
   });
   // sessionStorage.removeItem("park_info")
 })
-
-
-// 댓글 시간 나타내기 
-function time2str(date) {
-  let today = new Date()
-  let time = (today - date) / 1000 / 60  // 분
-
-  if (time < 60) {
-    return parseInt(time) + "분 전"
-  }
-  time = time / 60  // 시간
-
-  if (time < 24) {
-    return parseInt(time) + "시간 전"
-  }
-  time = time / 24
-
-  if (time < 7) {
-    return parseInt(time) + "일 전"
-  }
-
-  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
-}
 
 
 // 토글에 공원 목록 붙이기

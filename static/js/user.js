619b1b2fd9a6e892faa60391bbb1bccbe5d3f505
GyeongSuKim <rla04722@naver.com>
2022-07-27 14:20:43 +0900

    Refactor : 전역함수 user.js 로 이동
    전역 함수들
    community.js => user.js 로 이동

--- static/js/user.js
+++ static/js/user.js
@@ -66,3 +66,57 @@ async function logout() {
 
   window.location.replace(`${frontendBaseUrl}login.html`)
 }
+
+
+// 로그인한 user.id 찾는 함수
+function parseJwt(token) {
+  var base64Url = localStorage.getItem("access")
+  if (base64Url != null) {
+    base64Url = base64Url.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(window.atob(base64).split("").map(
+      function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(""));
+    return JSON.parse(jsonPayload);
+  }
+};
+
+
+// 로그인 여부 확인 후 드롭박스 변경
+const loginbtn = document.getElementById("my-login")
+const favorite = document.getElementById("my-favorite")
+const article = document.getElementById("my-article")
+const account = document.getElementById("my-account")
+
+if (parseJwt("access") != null) {
+  loginbtn.innerText = "로그아웃"
+} else {
+  favorite.style.display = "none"
+  article.style.display = "none"
+  account.style.display = "none"
+  loginbtn.innerText = "로그인"
+}
+
+
+// 시간 함수
+function time2str(date) {
+  let today = new Date()
+  let time = (today - date) / 1000 / 60  // 분
+
+  if (time < 60) {
+    return parseInt(time) + "분 전"
+  }
+  time = time / 60  // 시간
+
+  if (time < 24) {
+    return parseInt(time) + "시간 전"
+  }
+  time = time / 24
+
+  if (time < 7) {
+    return parseInt(time) + "일 전"
+  }
+
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+};

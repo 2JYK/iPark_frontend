3d6934767e3408e951184697456aabe768685494
GyeongSuKim <rla04722@naver.com>
2022-07-28 22:05:38 +0900

    Feat : modal 스크롤 방지, 이미지 미리보기

--- static/js/community.js
+++ static/js/community.js
@@ -25,23 +25,29 @@ if (parseJwt("access") != null) {
 
 // 모달
 $(function () {
+  const body = document.querySelector('body');
   $("#modal-open").click(function () {
+    body.style.overflow = "hidden"  // 스크롤 방지
     $("#popup").css('display', 'flex').hide().fadeIn();
   });
-
+  
   $("#close").click(function () {
+    body.style.overflow = "initial" // 스크롤 방지 해제
     modalClose();
   });
-
+  
   function modalClose() {
     $("#popup").fadeOut();
   }
 });
 
+
 // 모달 onclick
 function open_modal() {
+  const body = document.querySelector('body');
+  body.style.overflow = "hidden"  // 스크롤 방지
   $("#popup").css('display', 'flex').hide().fadeIn();
-
+  
   //수정 모달 안에 원래 있던 내용 넣어주기
   const old_title = document.getElementById("article_title")
   const old_content = document.getElementById("article_content")
@@ -49,11 +55,25 @@ function open_modal() {
   const input_content = document.getElementById("popup-body-content")
   input_title.value = old_title.innerHTML
   input_content.value = old_content.innerHTML
-
+  
   $("#close").click(function () {
+    body.style.overflow = "initial" // 스크롤 방지 해제
     modalClose();
   });
   function modalClose() {
     $("#popup").fadeOut();
+  }
+}
+
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById("preview").src = e.target.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    document.getElementById("preview").src = "";
   }
 }

f1c5d9f06341f5120efdb36f5fc55a94f7acd009
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-29 22:55:26 +0900

    Feat : refresh token 활용 함수 구현
    - 단점은 새로고침을 해야 적용이 된다는 것이다.
    - 추후 자동으로 실행될 수 있도록 수정할 예정

--- static/js/api_user.js
+++ static/js/api_user.js
@@ -66,6 +66,41 @@ async function handleLogin() {
 }
 
 
+// refresh token으로 access token 발급
+function refreshToken() {
+  const payload = JSON.parse(localStorage.getItem("payload"));
+
+  // 아직 access 토큰의 인가 유효시간이 남은 경우
+  if (payload.exp > (Date.now() / 1000)) {
+    return;
+  } else {
+    // `${backendBaseUrl}user/api/token/refresh/`
+    // 인증 시간이 지났기 때문에 다시 refresh token으로 access token 다시 요청
+    const requestRefreshToken = async (url) => {
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+          "refresh": localStorage.getItem("refresh")
+        })
+      }
+      );
+      return response.json();
+    };
+
+    // 다시 인증 받은 accessToken을 localStorage에 저장하자.
+    requestRefreshToken(`${backendBaseUrl}user/api/token/refresh/`).then((data) => {
+      // 새롭게 발급 받은 accessToken을 localStorage에 저장
+      const accessToken = data.access;
+      localStorage.setItem("access", accessToken);
+    });
+  }
+};
+refreshToken()
+
+
 // 카카오 로그인 : 토큰과 페이로드 생성, 회원가입 유도 //
 async function kakaoUserForm(authObj, kakaoData) {
   const kakaoUserData = Object.assign({}, authObj, kakaoData)

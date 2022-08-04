a010f763f7837d461ac4092faecebd68f55f3fbb
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-08-04 15:22:03 +0900

    Update : 도메인 변경

--- static/js/api.js
+++ static/js/api.js
@@ -6,5 +6,5 @@ TOKEN = {
 
 // const backendBaseUrl = "http://127.0.0.1:8000"
 // const frontendBaseUrl = "http://127.0.0.1:5500"
-const backendBaseUrl = "https://www.ilovepark.net"
-const frontendBaseUrl = "https://front.ilovepark.net"
+const backendBaseUrl = "https://front.ilovepark.net"
+const frontendBaseUrl = "https://www.ilovepark.net"

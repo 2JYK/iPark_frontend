c516fed494da462d86ca9482241e15d6e2957ca2
GyeongSuKim <rla04722@naver.com>
2022-08-09 23:05:49 +0900

    Feat : HTML 정규식 전역함수 선언

--- static/js/api.js
+++ static/js/api.js
@@ -1,10 +1,21 @@
+// const backendBaseUrl = "http://127.0.0.1:8000"
+// const frontendBaseUrl = "http://127.0.0.1:5500"
+const backendBaseUrl = "https://front.ilovepark.net"
+const frontendBaseUrl = "https://www.ilovepark.net"
+
+
 TOKEN = {
   "Access-Control-Allow-Origin": "*",
   "Authorization": "Bearer " + localStorage.getItem("access"),
 }
 
 
-// const backendBaseUrl = "http://127.0.0.1:8000"
-// const frontendBaseUrl = "http://127.0.0.1:5500"
-const backendBaseUrl = "https://front.ilovepark.net"
-const frontendBaseUrl = "https://www.ilovepark.net"
+// HTML 정규식
+function removeHTMLText(text) {
+  text = text.replaceAll(/<[^>]*>?/g, "");  // XSS 치한
+
+  // text = text.replaceAll("<","&lt;")
+  // text = text.replaceAll(">","&gt;")
+  // text = text.replaceAll("&", "&amp;")
+  return text
+}
